db714c2c78cfc53b189782e84ae5b9f4beb131a2
GodWasaProgrammer <bwnils@gmail.com>
2022-12-24 05:52:19 +0100

    created the basis for hangman game

--- Program.cs
+++ Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+
+            // makes a list of words for our hangman game //
+
+            List<string> Words = new()
+            {
+                "poland",
+                "mother",
+                "left",
+                "right",
+                "down"
+            };
+
+            // makes a random to pick a word from the list
+            Random wordPickerIndex = new Random();
+            // creates a variable that stores our indexposition chosen by our random, taking words total count as maxvalue
+            var pickIndex = wordPickerIndex.Next(Words.Count);
+            // picks the word randomly
+
+            string currentHangManWord = Words[pickIndex];
+
+            Console.WriteLine(currentHangManWord);
+
+            Console.ReadKey();
         }
+
     }
 }
